c726e9522eabf9134d9c0958a481617db6cbe298
Sujith P <sujithp2690@gmail.com>
2025-05-05 19:12:02 +0530

    skelton loader added

--- src/components/HomePage/Packages.jsx
+++ src/components/HomePage/Packages.jsx
@@ -6,18 +6,20 @@ import { packageApi } from '../../APIs/authAPI'
 const Packages = () => {
     const [packages, setPackages] = useState([])
 
-    useEffect(() => {
-        const getAllPackages = async () => {
-            try {
-                const { data } = await packageApi()
-                setPackages(data?.packages || []) // Ensure packages is always an array
-            } catch (error) {
-                console.error('Error fetching packages data:', error)
-                setPackages([]) // Fallback to empty array in case of error
-            }
+    const getAllPackages = async () => {
+        try {
+            const { data } = await packageApi()
+            console.log('----------its here')
+            setPackages(data?.packages || []) // Ensure packages is always an array
+        } catch (error) {
+            console.error('Error fetching packages data:', error)
+            setPackages([]) // Fallback to empty array in case of error
         }
-        getAllPackages()
-    }, [])
+    }
+    useEffect(() => {
+        console.log("useEffect triggered");
+        getAllPackages();
+    }, []);
 
 
     return (
@@ -44,9 +46,21 @@ const Packages = () => {
                         </div>
                     ))
                 ) : (
-                    <div className="col-span-full text-center text-gray-500 py-10">
-                        No Packages Available
-                    </div>
+                    // <div className="col-span-full text-center text-gray-500 py-10">
+                    //     No Packages Available
+                    // </div>
+                    <>
+                        {Array(3).fill().map((_, index) => (
+                            <div key={index} className="flex flex-col rounded shadow-md w-60 sm:w-72 animate-pulse h-full hover:shadow-2xl hover:border-yellow-500 dark:border-gray-700 border-4 ease-in-out duration-300">
+                                <div className="h-48 rounded-t dark:bg-gray-700"></div>
+                                <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-900">
+                                    <div className="w-full h-7 rounded dark:bg-gray-700"></div>
+                                    <div className="w-full h-7 rounded dark:bg-gray-700"></div>
+                                    <div className="w-3/4 h-7 rounded dark:bg-gray-700"></div>
+                                </div>
+                            </div>
+                        ))}
+                    </>
                 )}
             </div>
         </section>
